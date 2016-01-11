2a1ab93ae45b97f0b0aa3c21cb9a98b97fe4d112
Lukelle <lukic.nemanja@gmail.com>
2016-01-11 18:46:24 +0100

    -- screenSnd --
    Remove redundant files

--- screenSnd/screenSnd.cpp
+++ screenSnd/screenSnd.cpp
@@ -1,11 +1,9 @@
 // screenSnd.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <stdio.h>
 
 
-int main()
-{
-    return 0;
+int main() {
+    return 0; 
 }
-
